13499461d2212e8df40a491ffa74792d2f1c7504
Paco Amoros Cubells <paco.amoros.cubells@gmail.com>
2013-03-29 18:36:05 +0100

    up

---
+++ MajorOfCity/src/com/github/pakirra/MajorOfCity/MajorOfCity.java
@@ -0,0 +1,20 @@
+package com.github.pakirra.MajorOfCity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MajorOfCity extends JavaPlugin {
+	
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+	
+
+}
